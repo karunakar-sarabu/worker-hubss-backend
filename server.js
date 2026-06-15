4ee1e0576710c9a086194f5be47f6ddcfbcfd2f1
Karunakar Sarabu <karunakarsarabu9@gmail.com>
2026-06-15 05:19:28 -0700

    fix render port

--- server.js
+++ server.js
@@ -1,20 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.get("/", (req, res) => {
-//   res.send("Daily Wage Connect API Running...");
-// });
-
-// const PORT = 5000;
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -49,7 +32,7 @@ app.get("/", (req, res) => {
     res.send("Daily Wage Connect API Running...");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
